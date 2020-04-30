ae019606de8fb7ad9adddd3e0fb9de186d497538
elly2178 <dumig1@bfh.ch>
2020-04-30 17:17:16 +0200

    worklist v2

--- mwl_server/app2.py
+++ mwl_server/app2.py
@@ -9,41 +9,48 @@ ma = Marshmallow(app)
 
 
 class Worklist():        
-    
-    def __init__(self, PatientID: int, PatientName: str, ScheduledProcedureStepSequence: list):
-        # check if patientId contains other char exept ints
-        
-        
-        try:
-            self.PatientID = int(PatientID)
-        except ValueError:
-            raise ValueError('PatientID contains invalid value ---> {}'.format(PatientID))
-        
-        #check if pat name is longer than 30 char and if the chars are part of the alphabet
-        try:
-            if (len(PatientName) <= 30 and PatientName.isalpha()):
-                self.PatientName = PatientName
-            else:
-                raise IOError
-        except IOError:
-            raise IOError('Patient Name must be maximum 30 Charactes long and contain only alphabetical signs ---> {}'.format(PatientName))
-        
-        # check if list is complete +-
-        try:
-            pass
-            #if [0]= str():
-                #self.ScheduledProcedureStepSequence = ScheduledProcedureStepSequence
-            #else: 
-                #raise TypeError
-        except TypeError:
-            raise TypeError('List fields are not corrrect --> {}'.format(ScheduledProcedureStepSequence))
-    
-    def inputValidationCheck(self, valueToCheck, requiredType, maxSize=None, isDate=False):
-        if type(valueToCheck) is requiredType:
-            return True
-        else:
-            raise ValueError("Value {} is not of type {}".format(str(valueToCheck),requiredType))
+    # set of rules that need to be respected
+    ruleset = [{ "PatientID": str, "constraints": { "maxlength": 64 } },
+           { "PatientName": str, "constraints": { "maxlength": 32 } },
+           { "Modality": str},
+           { "AETitle": str, "constriants": {"maxlength": 16}},
+           {" ProcedureStepStartDate": int},
+           {" ProcedureStepStartTime": int},
+           {" PhysicianName": str}]
+           
+    def __init__(self, PatientID: str, PatientName: str, ScheduledProcedureStepSequence: list):
+        self.PatientID = PatientID
+        self.PatientName = PatientName
+        self.ScheduledProcedureStepSequence = ScheduledProcedureStepSequence        
+        self.porperties_that_we_need_to_check = {}
+        #json = {"PatientID": 55, "PatientName": 12345678}
+        for key, value in { "PatientID": self.PatientID, "PatientName": self.PatientName}.items():
+            for rule in Worklist.ruleset:
+                if key in rule.keys():
+                    self.check_value(key, value, rule[key], **rule["constraints"])   
 
+    def check_value(self, key, value, requiredType, **kwargs):
+        # chekcing for value type
+        if not isinstance(value, requiredType):
+            raise TypeError("Value for key {} not of type {}".format(key,str(requiredType)))
+        
+        for additional_constraint, additional_constraint_value in kwargs.items():
+            #  
+            if additional_constraint == "maxsize":
+                # if pat id co --> give error back
+                if value > additional_constraint_value:
+                    raise ValueError("Value for key {} bigger than {}".format(key, additional_constraint_value))
+            if additional_constraint == "maxlength":
+                if len(value) > additional_constraint_value:
+                    raise ValueError("Value for key {} longer than {}".format(key, additional_constraint_value))
+            # pat name must contain chars and not numbers: if (len(PatientName) <= 30 and PatientName.isalpha())
+        return True
+
+
+
+
+
+ 
 class WorklistSchema(ma.Schema):
     class Meta:
         fields = ('PatientID', 'PatientName', 'ScheduledProcedureStepSequence')
@@ -52,27 +59,16 @@ worklist_schema = WorklistSchema()
 
 @app.route('/wl', methods = ['POST'])
 def create_wl():
-    
     PatientID = request.json['PatientID']
     PatientName = request.json['PatientName']
     ScheduledProcedureStepSequence = request.json['ScheduledProcedureStepSequence']
-    try:
-        worklist_created = Worklist(PatientID, PatientName, ScheduledProcedureStepSequence)
-        
-    except ValueError as error:
-        return jsonify(str(error), 400)
-
-    except IOError as anothererror:
-        return jsonify(str(anothererror),400)
-
-    except TypeError as aError:
-        return jsonify(str(aError), 400)
+    
+    worklist_created = Worklist(PatientID, PatientName, ScheduledProcedureStepSequence)
+    
     retval = worklist_schema.jsonify(worklist_created)
     return retval
-    
-     
+
+
+                
 if __name__ == "__main__":
-    app.run(debug = True)
-
-
-   
+    app.run(debug = True)  
