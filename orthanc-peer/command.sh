af948e7872c29168344a24137480959b1de741e0
armon dressler <armon.dressler@gmail.com>
2020-05-06 23:17:22 +0200

    C-Find correctly forwarded to /enhancequery endpoint

--- orthanc-peer/command.sh
+++ orthanc-peer/command.sh
@@ -3,4 +3,5 @@ docker run -p 4242:4242 -p 8042:8042 --rm \
   -v $PWD/orthanc-peer/orthanc.json:/etc/orthanc/orthanc.json:ro \
   -v $PWD/orthanc-peer/sample.py:/etc/orthanc/sample.py:ro \
   -v $PWD/orthanc-peer/intercept_c_find.lua:/etc/orthanc/intercept_c_find.lua:ro \
+  -v $PWD/orthanc-peer/libNanoPlugin.so:/usr/local/share/orthanc/plugins/libNanoPlugin.so:ro \
   jodogne/orthanc-python
