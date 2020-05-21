aa99adc9434716584551043131f8c2b381941f26
armon dressler <armon.dressler@gmail.com>
2020-05-21 03:11:02 +0200

    removed old orthanc-peer structure, code moved to orthanc-plugins

--- orthanc-peer/command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run -p 4242:4242 -p 8042:8042 --rm \
-  -v $PWD/orthanc-peer/orthanc.json:/etc/orthanc/orthanc.json:ro \
-  -v $PWD/orthanc-peer/sample.py:/etc/orthanc/sample.py:ro \
-  -v $PWD/orthanc-peer/libNanoPlugin.so:/usr/local/share/orthanc/plugins/libNanoPlugin.so:ro \
-  dumig1/orthanc-python:20-05-rc2
