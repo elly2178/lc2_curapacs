9fe3f480721cfe67dc37f292d7f59b8e2496eae3
armon dressler <armon.dressler@gmail.com>
2020-05-21 03:06:54 +0200

    unifying orthanc plugins in /orthanc-plugins

--- mwl_server/command.sh
+++ mwl_server/command.sh
@@ -2,5 +2,5 @@
 [ ! -d ./mwl_server ] && { echo "make sure to run this command from the lc2_curapacs directory, exiting"; exit 1; }
 docker run -p 4242:4242 -p 8042:8042 --rm \
   -v $PWD/mwl_server/orthanc.json:/etc/orthanc/orthanc.json:ro \
-  -v $PWD/mwl_server/app.py:/etc/orthanc/app.py:ro \
+  -v $PWD/mwl_server/app.py:/usr/local/share/orthanc/app.py:ro \
   dumig1/orthanc-python:20-05-rc2
---
+++ orthanc-plugins/command.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker run -p 4242:4242 -p 8042:8042 --rm \
+  -v $PWD/orthanc-plugins/orthanc.json:/etc/orthanc/orthanc.json:ro \
+  -v $PWD/orthanc-plugins/main.py:/usr/local/share/orthanc/plugins/main.py:ro \
+  -v $PWD/orthanc-plugins/curapacs_python:/usr/local/share/orthanc/plugins/curapacs_python:ro \
+  -v $PWD/_orthanc-peer/libNanoPlugin.so:/usr/local/share/orthanc/plugins/libNanoPlugin.so:ro \
+  dumig1/orthanc-python:20-05-rc2
