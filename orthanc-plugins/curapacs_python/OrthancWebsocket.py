41b8ac072721b64eb8662ab57a614124c45e3e11
armon dressler <armon.dressler@gmail.com>
2020-06-02 17:51:25 +0200

    -

--- orthanc-plugins/curapacs_python/OrthancWebsocket.py
+++ orthanc-plugins/curapacs_python/OrthancWebsocket.py
@@ -30,36 +30,42 @@ async def consumer_handler(websocket, path):
 
 async def OrthancUnixSocketHandler(reader, writer):
     config.LOGGER.debug(f"OrthancUnixSocketHandler started.")
-    data = await reader.read()
-    try:
-        data = data.decode()
-    except UnicodeDecodeError:
-        config.LOGGER.error(f"Failed to decode bytestring from unix socket")
-        return
-    config.LOGGER.debug(f"OrthancUnixSocketHandler forwarding message to all connected orthancs: {data}")
-    await OrthancMessaging.queue.put(data)
+    async for message in reader:
+        data = await reader.read()
+        try:
+            data = data.decode()
+        except UnicodeDecodeError:
+            config.LOGGER.error(f"Failed to decode bytestring from unix socket")
+            return
+        config.LOGGER.debug(f"OrthancUnixSocketHandler forwarding message to all connected orthancs: {data}")
+        await OrthancMessaging.queue.put(data)
 
 async def OrthancMessageHandlerClient(uri):
-    print("STARTING OrthancMessageHandlerClient")
+    config.LOGGER.debug(" OrthancMessageHandlerClient")
     auth_header = list(helpers.get_http_auth_header(config.PEER_HTTP_USER, config.PEER_HTTP_PASSWORD).items())[0]
-    async with websockets.connect(uri, extra_headers=[auth_header]) as websocket_client:
-        async for message in websocket_client:
-            print("GOT MESSAGE: " + message)
+    while True:
+        config.LOGGER.debug(f"Websocket client connecting to {uri}")
+        async with websockets.connect(uri, extra_headers=[auth_header]) as websocket_client:
+            async for message in websocket_client:
+                print("GOT MESSAGE: " + message)
     print("ENDING OrthancMessageHandlerClient")
 
 async def OrthancMessageHandler(websocket, path):
     OrthancMessaging.connected_instances.add(websocket)
-    consumer_task = asyncio.ensure_future(
-        consumer_handler(websocket, path))
-    producer_task = asyncio.ensure_future(
-        producer_handler(websocket, path))
-    done, pending = await asyncio.wait(
-        [consumer_task, producer_task],
-        timeout=config.HTTP_TIMEOUT,
-        return_when=asyncio.FIRST_COMPLETED,
-    )
-    for task in pending:
-        task.cancel()
+    try:
+        consumer_task = asyncio.ensure_future(
+            consumer_handler(websocket, path))
+        producer_task = asyncio.ensure_future(
+            producer_handler(websocket, path))
+        done, pending = await asyncio.wait(
+            [consumer_task, producer_task],
+            timeout=config.HTTP_TIMEOUT,
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        for task in pending:
+            task.cancel()
+    finally:
+        OrthancMessaging.connected_instances.remove(websocket)
 
 
 event_loop = asyncio.get_event_loop()
@@ -71,8 +77,7 @@ else:
     config.LOGGER.info("Starting websocket client.")
     event_loop.create_task(OrthancMessageHandlerClient("ws://c0100-orthanc.curapacs.ch/ws"))
 unix_server = asyncio.start_unix_server(OrthancUnixSocketHandler, path=config.LOCAL_UNIX_SOCKET_PATH)
-event_loop.run_until_complete(unix_server)
-print("TODO STARTING SECONDARY PROCESS")
-ORTHANC_WEBSOCKET_PROCESS = multiprocessing.Process(target=event_loop.run_forever)
+event_loop.create_task(unix_server)
+ORTHANC_WEBSOCKET_PROCESS = multiprocessing.Process(target=event_loop.run_forever, name="orthanc async manager")
 ORTHANC_WEBSOCKET_PROCESS.daemon = True
 ORTHANC_WEBSOCKET_PROCESS.start()
--- orthanc-plugins/curapacs_python/deleteme.py
+++ orthanc-plugins/curapacs_python/deleteme.py
@@ -1,34 +1,18 @@
 import asyncio
 import websockets
 
-
-queue = asyncio.Queue()
-queue.put_nowait("FUBAR")
-
-async def producer_handler(websocket, path):
-    while True:
-        message = await queue.get()
-        await websocket.send(message)
-
-async def consumer_handler(websocket, path):
+async def consumer_handler(reader, writer):
     print("NEW CONNECTION")
-    async for message in websocket:
-        print("MESSAGE RECEIVED: " + message)
+    async for message in reader:
+        print("MESSAGE RECEIVED: " + message.decode())
 
-
-async def handler(websocket, path):
-    consumer_task = asyncio.ensure_future(
-        consumer_handler(websocket, path))
-    producer_task = asyncio.ensure_future(
-        producer_handler(websocket, path))
-    done, pending = await asyncio.wait(
-        [consumer_task, producer_task],
-        return_when=asyncio.FIRST_COMPLETED,
-    )
-    print("DONE: " + str(done) + " PENDING: " + str(pending))
-    for task in pending:
-        task.cancel()
     
+loop = asyncio.get_event_loop()
+unix_server = asyncio.start_unix_server(consumer_handler, path="/tmp/fuckme.sock")
+loop.create_task(unix_server)
+loop.run_forever()
+
+
 """
 start_server = websockets.serve(handler, "0.0.0.0", 8081)
 loop = asyncio.get_event_loop()
